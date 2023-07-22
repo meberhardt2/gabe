661f5f35e9d02fc86facc59152eefb3a35e6aad7
unknown <matte@fastmail.com>
2023-07-21 19:02:43 -0500

    work

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,7 +1,9 @@
 /*************************************************************/
 var title = "calculator project";
 var header = "A Simple Calculator1";
-var number_pushed;
+var number_pushed1 = '';
+var number_pushed2 = '';
+var what_step = 'step1';
 /*************************************************************/
 
 
@@ -14,33 +16,76 @@ function init(){
 
 
 /*************************************************************/
-function button_click(element){
+function number_click(element){
 	//console.log(this.innerHTML);
 	//getting number that was clicked or tapped
-	number_pushed = this.innerHTML;
+	if(what_step === 'step1'){
+		number_pushed1 = number_pushed1 + this.innerHTML;
+		display_change(number_pushed1);	
+	}
+
+	if(what_step === 'step2'){
+		number_pushed2 = number_pushed2 + this.innerHTML;
+		display_change(number_pushed2);	
+	}
 }
 /*************************************************************/
 
 
-function equals(){
-	console.log(number_pushed);
+/*************************************************************/
+function calc_functions(){
+	var function_pushed = this.innerHTML;
+	
+	if (function_pushed === '+'){
+		what_step = 'step2';
+	}
+	if (function_pushed === '-'){
+		what_step = 'step2';
+
+	}
+	if (function_pushed === '*'){
+		what_step = 'step2';
+
+	}
+	if (function_pushed === '/'){
+		what_step = 'step2';
+
+	}
+	if (function_pushed === '='){
+
+	}
+	if (function_pushed === 'C'){
+		number_pushed1 = '';
+		number_pushed2 = '';
+		what_step = 'step1';
+		display_change('0');
+	}
 }
+/*************************************************************/
+
+
+/*************************************************************/
+function addlistener(){
+	for(var i = 0; i < document.getElementsByClassName('calc-number').length; i++){
+		document.getElementsByClassName('calc-number')[i].addEventListener('click', number_click);
+	}
+
+	for(var x = 0; x < document.getElementsByClassName('calc-function').length; x++){
+		document.getElementsByClassName('calc-function')[x].addEventListener('click',calc_functions);
+	}
+}
+/*************************************************************/
+
+/**************************************************************/
+function display_change(display_number){
+	document.getElementById('number-display').innerHTML = display_number;
+}
+/**************************************************************/
+
 
 
 /*************************************************************/
 init();
-
-document.getElementsByClassName('calc-number')[0].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[1].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[2].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[3].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[4].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[5].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[6].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[7].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[8].addEventListener('click', button_click);
-document.getElementsByClassName('calc-number')[9].addEventListener('click', button_click);
-
-document.getElementsByClassName('calc-function')[4].addEventListener('click', equals);
+addlistener();
 /*************************************************************/
 
