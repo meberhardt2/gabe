d5b007e8972cff5f57e510f3bc7b53f9eaefd11e
geberhardt2 <gabe.eberhardt@gmail.com>
2023-07-31 19:08:12 -0500

    finished project two. start of future project

---
+++ src/js/AutoClicker.js
@@ -0,0 +1,23 @@
+/*********************************/
+var speed = '';
+
+/*********************************/
+if(speed === 1){
+    setTimeout(clicky(), 1000);
+}
+
+if(speed === 0.1){
+    setTimeout(clicky(), 1);
+}
+/*********************************/
+function speed_selection(){
+    for(var i = 0; i < document.getElementsByClassName('speed-number'.length) ;i++){
+        document.getElementsByClassName('speed-number')['i'].addEventListener('click', number_click);
+        speed = 'speed-number';}  
+}
+/*********************************/
+function clicky(){
+    element.click(1)
+}
+/*********************************/
+speed_selection();
--- src/js/guessing.js
+++ src/js/guessing.js
@@ -1,6 +1,7 @@
 var settings = {
     guesses_left: 20,
     title: 'Guessing game',
+    R_Num: 0
 };
 
 
@@ -11,6 +12,8 @@ function setup(){
 
     document.getElementById('guess').addEventListener('click',check_number);
 
+    settings.R_Num = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+    console.log(settings.R_Num)
 }
 /****************************/
 
@@ -18,7 +21,27 @@ function setup(){
 /****************************/
 function check_number(){
     var guessed_number = document.getElementById('guessing-number').value;
-    console.log(guessed_number);
+
+    if(parseInt(guessed_number,10) === parseInt(settings.R_Num,10)){
+        document.getElementById('message').innerHTML = 'You won! The number was ' + settings.R_Num; 
+    }
+   
+    if(guessed_number < settings.R_Num){
+        document.getElementById('message').innerHTML = "You're too low";
+        settings.guesses_left = settings.guesses_left - 1;
+        document.getElementById('how-many-tries').innerHTML = settings.guesses_left;
+    }
+    
+    if(guessed_number > settings.R_Num){
+        document.getElementById('message').innerHTML = "You're too high"; 
+        settings.guesses_left = settings.guesses_left - 1;
+        document.getElementById('how-many-tries').innerHTML = settings.guesses_left;
+    }
+    if(settings.guesses_left === 0){
+        document.getElementById('guess').disabled = true;
+        document.getElementById('message').innerHTML = "You've lost!"; 
+    }
+    document.getElementById('guessing-number').focus()
 }
 /****************************/
 
